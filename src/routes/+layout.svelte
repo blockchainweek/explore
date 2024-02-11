081dc5bb34d91cfea67efa74da4f4d2e6cedb553
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-02-11 18:12:10 +0100

    Rename pbw to bbw. Change colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,22 +48,18 @@
 	{#if homepage}
 		<div class="px-6 xl:px-0">
 			<div class="max-w-7xl mx-auto pt-10 pb-6">
-				<h1 class="text-4xl font-bold text-bbw-white">
+				<h1 class="text-5xl uppercase font-bold text-bbw-white">
 					<a href="/">Berlin Blockchain Week 2024</a>
 				</h1>
-				<div class="text-2xl text-bbw-yellow">May 18-27, 2024</div>
+				<div class="text-4xl text-bbw-yellow">May 18-27, 2024</div>
 			</div>
 		</div>
 		<div class="bg-white dark:bg-bbw-dark px-6 xl:px-0 pt-6 pb-4">
 			<div class="max-w-7xl mx-auto bg-bbw-navy/10 text-bbw-navy dark:text-gray-300 rounded-lg">
-				<div class="p-5 text-lg">
+				<div class="p-5 text-xl">
 					<p>
 						Berlin Blockchain Week is a decentralized community-organized initiative. There is no
-						single owner. It is an agnostic movement based on the premise that self-organization is
-						the backbone of the ecosystem. We invite projects, individuals and anyone interested in
-						fruitful and educational discussions to join us in Berlin in May.<br /><br />
 						We are committed to reduce the noise from the market and focus on education, in order to
-						push mass adoption by providing first-hand information about the industry.<br /><br />
 						Our concept is simple: keep it educational, try to be as cost-friendly for the community
 						as you can (bearing in mind, we all need to pay bills - so not all events are free!) and
 						to create a welcoming and inclusive environment, so interested parties at anyknowledge level
